e9a272ff9dd3aeed9e664a18ad529df0337dc068
amalia barrigas <amaliabarrigas@gmail.com>
2025-06-09 16:33:36 +0200

    Ruta get api/users/:id funciona

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,5 +1,5 @@
 const router=require("express").Router()
-
+const User = require('../models/User.model');
 
 router.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
@@ -13,13 +13,24 @@ const verifyToken = require("../middlewares/auth.middlewares")
 
 //GET /api/users/:id
 router.get("/users/:userId", verifyToken, (req, res, next) => {
+  const { userId } = req.params;
+
+    if (req.payload._id !== userId) {
+    return res.status(403).json({ message: "No autorizado" });
+  }
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+      res.status(200).json(user);
+    })
+    .catch((error) => next(error));
+});
+  
   
   //TODO LA LOGICA QUE TIENE QE ENVIAR LA RESPUESTA DE UN USUARIO EN CONCRETO
   //TODO SIMILAR A UNIR CANCIONES CON ARTISTAS.
 
-  console.log(req.payload)
-  res.send("esta info es para usuarios que ya están logueados")
-})
 
 
 const studentRouter = require ("./student.routes")
